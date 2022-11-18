3c91bfa5ff0f3a272b6395e0084afa95091494d3
Sabarno Chatterjee <sabarno.05@gmail.com>
2022-11-18 06:14:35 +0530

    Edit main.py

--- main.py
+++ main.py
@@ -1,60 +1,42 @@
-from art import logo
+import art
+print(art.logo)
 
-#Add
 def add(n1, n2):
-    """This function is used for addition."""
-    return n1 + n2
+  return n1 + n2
 
-
-#Subtract
 def subtract(n1, n2):
-    """This function is used for subtraction."""
-    return n1 - n2
+  return n1 -  n2
 
-
-#Multiplication
 def multiply(n1, n2):
-    """This function is used for multiplication."""
-    return n1 * n2
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1/n2
 
 
-#Division
-def division(n1, n2):
-    """This function is used for division."""
-    return n1 / n2
+def calculations():
+  n1 = float(input("\nWhats the first number?\n"))
+  
+  more_operations = True
+  
+  while more_operations:
+      operator = input("\nWhats the operator? ('+', '-', '*', '/')\n")
+      n2 = float(input("\nWhats the next number?\n"))
+      
+      calci = {
+        "+" :  add(n1,n2),
+        "-" :  subtract(n1, n2),
+        "*" :  multiply(n1, n2),
+        "/" :   divide(n1, n2),
+        }
+      
+      operation = calci[operator]
+      print(f"\n{n1} {operator} {n2} = {operation}")
+      n1 = operation
+      if input(("\nPress enter to perform calculations with the same number or press N to start a new calculator.").lower()) == "n":
+        more_operations = False
+        calculations()
 
-
-operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": division,
-}
-
-def calculator():
-    """This function is used for the purpose of calculations."""
-    print(logo)
-    num1 = float(input("What's the first number?\n"))
-
-    for symbol in operations:
-        print(symbol, end='  ')
-    print()
-
-
-
-    should_continue = True
-    while should_continue:
-        operation_symbol = input("Pick an operation from the line above.\n")
-        num2 = float(input("What's the next number?\n"))
-        print()
-
-        calculation_function = operations[operation_symbol]
-        answer = calculation_function(num1, num2)
-        print(f"{num1} {operation_symbol} {num2} = {answer}")
-        if input("Do you want to continue? Press 'y' or 'n'.") == "y":
-            num1 = answer
-        else:
-            should_continue = False
-            calculator()
-
-calculator()
+calculations()
+      
+  
