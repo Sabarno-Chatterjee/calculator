ddc0367d50a518a7448181fabe6062b4f0de8de0
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-02-11 17:42:39 +0530

    layout

--- main.py
+++ main.py
@@ -41,12 +41,15 @@
 
 
 from tkinter import *
+
 # Application Constants:
-HEIGHT = 400
-WIDTH = 300
+HEIGHT = 410
+WIDTH = 310
 window_bg = "#D8D9CF"
 screen_bg = "#EDEDED"
 black = "#000000"
+white = "#FFFFFF"
+
 
 # Setting up window and icon:
 window = Tk()
@@ -56,12 +59,57 @@ window.iconphoto(False, icon)
 window.config(padx=10, pady=10, bg=window_bg)
 
 # Screen canvas:
-screen_canvas = Canvas(width=300, height=60, bg=screen_bg)
-screen_canvas.grid(row=0, column=0)
+screen_canvas = Canvas(width=300, height=70, bg=screen_bg)
+screen_canvas.grid(row=0, column=0, columnspan=4, pady=10)
 
+seven_button = Button(width=4, height=1, text="7", font=("arial", 20,"bold"), bg=black, fg=white)
+seven_button.grid(row=1, column=0)
 
+eight_button = Button(width=9, height=3)
+eight_button.grid(row=1, column=1)
 
+nine_button = Button(width=9, height=3)
+nine_button.grid(row=1, column=2)
 
+divide_button = Button(width=9, height=3)
+divide_button.grid(row=1, column=3)
+
+# 456* 123- 0C=+
+four_button = Button(width=9, height=3)
+four_button.grid(row=2, column=0)
+
+five_button = Button(width=9, height=3)
+five_button.grid(row=2, column=1)
+
+six_button = Button(width=9, height=3)
+six_button.grid(row=2, column=2)
+
+multiply_button = Button(width=9, height=3)
+multiply_button.grid(row=2, column=3)
+
+one_button = Button(width=9, height=3)
+one_button.grid(row=3, column=0)
+
+two_button =Button(width=9, height=3)
+two_button.grid(row=3, column=1)
+
+three_button =Button(width=9, height=3)
+three_button.grid(row=3, column=2)
+
+minus_button =Button(width=9, height=3)
+minus_button.grid(row=3, column=3)
+
+zero_button =Button(width=9, height=3)
+zero_button.grid(row=4, column=0)
+
+clear_button =Button(width=9, height=3)
+clear_button.grid(row=4, column=1)
+
+equals_button =Button(width=9, height=3)
+equals_button.grid(row=4, column=2)
+
+add_button = Button(width=9, height=3)
+add_button.grid(row=4, column=3)
 # screen_canvas = Canvas(width=320, height=65, bg=screen2_bg, highlightthickness=0)
 # screen_canvas.pack()
 
